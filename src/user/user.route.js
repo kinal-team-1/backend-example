c3c39138682ca3fd9d0779fad35c7361882ea0c3
jcastillo21 <bcastillo-2022474@kinal.edu.gt>
2024-05-29 20:36:21 -0600

    fix: force update

--- src/user/user.route.js
+++ src/user/user.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { getAllUsers, createUser, deleteUserById } from "./user.controller.js";
-import { body, query, param } from "express-validator"; // the 3 most important methods from express-validator
+import { createUser, getAllUsers } from "./user.controller.js";
+import { body, query } from "express-validator"; // the 3 most important methods from express-validator
 import { validateChecks } from "../middleware/validate-checks.js";
 import { retrieveLocale } from "../middleware/retrieve-locale.js";
 import User, { ACTIVE } from "./user.model.js";
@@ -59,40 +59,28 @@ router
     createUser,
   );
 
-router
-  .route("/:id")
-  .put([
-    retrieveLocale,
-    body("email")
-      .optional()
-      .isEmail()
-      .withMessage(message((LL) => LL.INVALID_OPTIONAL_USER_EMAIL())), // If last check fails, this message will be shown
-    body("password")
-      .optional()
-      .isStrongPassword({
-        minLength: 8,
-        minLowercase: 1,
-        minUppercase: 1,
-        minNumbers: 1,
-        minSymbols: 1,
-      })
-      .withMessage(message((LL) => LL.INVALID_OPTIONAL_USER_PASSWORD())),
-    body("name")
-      .optional()
-      .isString()
-      .isLength({ min: 3, max: 255 })
-      .withMessage(message((LL) => LL.INVALID_OPTIONAL_USER_NAME())),
-    validateChecks,
-  ])
-  .delete(
-    [
-      retrieveLocale,
-      param("id")
-        .isMongoId()
-        .withMessage(message((LL) => LL.INVALID_MONGO_ID())),
-      validateChecks,
-    ],
-    deleteUserById,
-  );
+router.route("/:id").put([
+  retrieveLocale,
+  body("email")
+    .optional()
+    .isEmail()
+    .withMessage(message((LL) => LL.INVALID_OPTIONAL_USER_EMAIL())), // If last check fails, this message will be shown
+  body("password")
+    .optional()
+    .isStrongPassword({
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    })
+    .withMessage(message((LL) => LL.INVALID_OPTIONAL_USER_PASSWORD())),
+  body("name")
+    .optional()
+    .isString()
+    .isLength({ min: 3, max: 255 })
+    .withMessage(message((LL) => LL.INVALID_OPTIONAL_USER_NAME())),
+  validateChecks,
+]);
 
 export default router;
